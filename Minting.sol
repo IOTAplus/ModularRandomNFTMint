7b7f8b8defb177484dd23015cdc72b9c68b319d2
MarlonMorales0 <95349381+MarlonMorales0@users.noreply.github.com>
2023-11-24 19:45:06 +0100

    Update Minting.sol

--- Minting.sol
+++ Minting.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MyNFT is ERC721Enumerable, Ownable {
+contract RandomMintNFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
     using EnumerableSet for EnumerableSet.UintSet;
 
@@ -39,7 +39,7 @@ contract MyNFT is ERC721Enumerable, Ownable {
 
     uint256 public totalNFTsSold;
     uint256 public mintPrice = 10000 * 10**18; // 10000 Sphericals
-    IERC20 public sphericalsToken = IERC20(0x70AE63bA0D88335c3CC89802AeEf253114cea30c);
+    IERC20 public sphericalsToken = IERC20(0xa662a27EC0EC79c1c75bf9BC4ff4aA1f1A7a27AF);
     address public paymentReceiver;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(msg.sender) {
@@ -96,7 +96,11 @@ contract MyNFT is ERC721Enumerable, Ownable {
             availableTypes.remove(typeId);
         }
     }
-
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+            require(_tokenExists[tokenId], "ERC721Metadata: URI query for nonexistent token");
+            uint256 nftTypeId = _tokenTypeIndexes[tokenId];
+            return nftTypes[nftTypeId].uri;
+        }
     function getRandomAvailableTypeIndex() private view returns (uint256) {
         uint256 totalMintableSupply = getTotalMintableSupply();
         uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % totalMintableSupply;
@@ -119,43 +123,86 @@ contract MyNFT is ERC721Enumerable, Ownable {
         }
         return totalMintableSupply;
     }
+   function getNFTTypes() public view returns (string memory) {
+    string memory json = '[';
+    for (uint256 i = 0; i < nftTypes.length; i++) {
+        uint256 remainingSupply = nftTypes[i].maxSupply - nftTypes[i].currentSupply;
+        uint256 ratio = (getTotalMintableSupply() > 0) ? (remainingSupply * 10000) / getTotalMintableSupply() : 0;
 
-    function getNFTTypes() public view returns (NFTTypeInfo[] memory) {
-        NFTTypeInfo[] memory typesInfo = new NFTTypeInfo[](nftTypes.length);
-        uint256 totalMintableSupply = getTotalMintableSupply();
+        json = string(abi.encodePacked(json,
+            '{',
+            '"name":"', nftTypes[i].name, '",',
+            '"maxSupply":', uint2str(nftTypes[i].maxSupply), ',',
+            '"remainingSupply":', uint2str(remainingSupply), ',',
+            '"ratioLeft":', uint2str(ratio), ',',
+            '"uri":"', nftTypes[i].uri, '"',  // Include the URI here
+            '}', 
+            (i < nftTypes.length - 1) ? ',' : ''
+        ));
+    }
+    return string(abi.encodePacked(json, ']'));
+}
+
+    function nFTsOfOwner(address owner) public view returns (string memory) {
+            string memory json = '[';
+            uint256 tokenCount = balanceOf(owner);
+            for (uint256 i = 0; i < tokenCount; i++) {
+                uint256 tokenId = tokenOfOwnerByIndex(owner, i);
+                uint256 typeId = _tokenTypeIndexes[tokenId];
+                NFTType memory nftType = nftTypes[typeId];
+                json = string(abi.encodePacked(json, 
+                    '{',
+                    '"tokenId":', uint2str(tokenId), ',',
+                    '"uri":"', nftType.uri, '",',
+                    '"nftType":"', nftType.name, '"',
+                    '}',
+                    (i < tokenCount - 1) ? ',' : ''
+                ));
+            }
+            return string(abi.encodePacked(json, ']'));
+        }
+
+    // Helper function to convert uint to string
+    function uint2str(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+    // Function to update the URI of an NFT type
+    function setNFTTypeURI(string memory nftTypeName, string memory newUri) public onlyOwner {
+        bool typeExists = false;
+        uint256 typeId;
+
+        // Search for the NFT type by name
         for (uint256 i = 0; i < nftTypes.length; i++) {
-            uint256 remainingSupply = nftTypes[i].maxSupply - nftTypes[i].currentSupply;
-            uint256 ratio = (totalMintableSupply > 0) ? (remainingSupply * 10000) / totalMintableSupply : 0;
-            typesInfo[i] = NFTTypeInfo({
-                name: nftTypes[i].name,
-                maxSupply: nftTypes[i].maxSupply, // Added maxSupply here
-                remainingSupply: remainingSupply,
-                ratioLeft: ratio
-            });
+            if (keccak256(abi.encodePacked(nftTypes[i].name)) == keccak256(abi.encodePacked(nftTypeName))) {
+                typeExists = true;
+                typeId = i;
+                break;
+            }
         }
-        return typesInfo;
-    }
 
-    function tokensOfOwner(address owner) public view returns (OwnedNFTInfo[] memory) {
-        uint256 tokenCount = balanceOf(owner);
-        OwnedNFTInfo[] memory ownedNFTs = new OwnedNFTInfo[](tokenCount);
-        for (uint256 i = 0; i < tokenCount; i++) {
-            uint256 tokenId = tokenOfOwnerByIndex(owner, i);
-            uint256 typeId = _tokenTypeIndexes[tokenId];
-            ownedNFTs[i] = OwnedNFTInfo({
-                tokenId: tokenId,
-                uri: nftTypes[typeId].uri,
-                nftType: nftTypes[typeId].name
-            });
-        }
-        return ownedNFTs;
-    }
+        // Ensure the NFT type exists
+        require(typeExists, "NFT type does not exist");
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_tokenExists[tokenId], "ERC721Metadata: URI query for nonexistent token");
-        uint256 nftTypeId = _tokenTypeIndexes[tokenId];
-        return nftTypes[nftTypeId].uri;
+        // Update the URI
+        NFTType storage nftType = nftTypes[typeId];
+        nftType.uri = newUri;
     }
-
-    // ... Additional functions and logic ...
 }
